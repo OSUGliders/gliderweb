caf8d4c48c79f3b9bd86da932c4d46a4051924de
Swathi Gangi <55583108+gangiswathi2000@users.noreply.github.com>
2025-11-25 00:45:41 -0800

    added static Url along with static root

--- trackglider/settings.py
+++ trackglider/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10 MB
+DATA_UPLOAD_MAX_MEMORY_SIZE = 11 * 1024 * 1024  # 10 MB

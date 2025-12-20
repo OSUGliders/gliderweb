56c667440bcda1ddd0dc83cc64e4484abbc8c95d
Swathi Gangi <gangiswathi2000@gmail.com>
2025-12-19 17:03:45 -0800

    Add home page and glider map templates, update URL routing and view functions

--- VisualizeMultipleGliders/urls.py
+++ VisualizeMultipleGliders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import glider_map_view, plot_glider_data, glider_data_to_display
+from .views import glider_map_view, plot_glider_data, glider_data_to_display,home_page
 
 urlpatterns = [
-     path('', glider_map_view, name='glider_map'),
+     path('', home_page, name='home_page'), 
+     path('map/', glider_map_view, name='glider_map'),
      path('plots/', plot_glider_data, name='plots'),
-     path('gliderdata/',glider_data_to_display, name='glider_data_to_display')
+     path('gliderdata/',glider_data_to_display, name='glider_data_to_display'),
+     
 ]
